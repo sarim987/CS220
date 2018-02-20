eed66b6e164b738188d1c9f59d7048e0346d481a
Sarim Ahmed <Sarim@Sarims-MacBook-Pro.local>
2018-02-19 21:15:12 -0500

    CSV Wrang finished

--- scala-csv-wrangling/src/main/scala/Main.scala
+++ scala-csv-wrangling/src/main/scala/Main.scala
@@ -45,26 +45,31 @@ object Main {
   def mostPopular(rows: List[SSARow]): (String, Int) = {
     rows.groupBy(item => item.name).mapValues(vals => vals.map(_.count).sum).maxBy(_._2)
   }
-  def findMax(rows: List[SSARow]): (String, Int) = {
-    def findMaxHelper(max: (String, Int), lis: List[SSARow]): (String, Int) = lis match {
-      case Nil => max
-      case item :: tail if item.name == max._1 => findMaxHelper((max._1, max._2 + item.count), tail)
-      case item :: tail if item.count > max._2 => findMaxHelper((item.name, item.count), tail)
-      case item :: tail => findMaxHelper((max._1, max._2), tail)
-    }
-    findMaxHelper(("", 0), rows)
+
+  def count(rows: List[SSARow]): Int = {
+    rows.map(_.count).sum
   }
 
-  def count(rows: List[SSARow]): Int = ???
+  def countGirlsAndBoys(rows: List[SSARow]): (Int, Int) = {
+    (rows.filter(item => item.gender == Female()).map(_.count).sum, rows.filter(item => item.gender == Male()).map(_.count).sum)
+  }
 
-  def countGirlsAndBoys(rows: List[SSARow]): (Int, Int) = ???
-
-  def genderNeutralNames(rows: List[SSARow]): Set[String] = ???
+  def genderNeutralNames(rows: List[SSARow]): Set[String] = {
+    rows.groupBy(item => item.name).mapValues(vals => vals.map(_.gender)).filter({case (a, b) => b == List(Male(), Female()) || b == List(Female(), Male())}).toList.map(_._1).toSet
+  }
 
   def expectedAlive(gender: Gender, birthYear: Int, currentYear: Int,
-    lifeExpectancies: List[CDCRow]): Boolean = ???
+    lifeExpectancies: List[CDCRow]): Boolean = {
+    val a = lifeExpectancies.filter(item => item.birthYear == birthYear)
+    if(a.length > 0){ 
+      val b = a.head
+      if(gender == Male()) (currentYear - birthYear) <= b.maleLifeExpectancy else (currentYear - birthYear) <= b.femaleLifeExpectancy
+    } else false
+  }
 
   def estimatePopulation(rows: List[SSARow], year: Int,
-    lifeExpectancies: List[CDCRow]): Int = ???
+    lifeExpectancies: List[CDCRow]): Int = {
+      count(rows.filter(item => expectedAlive(item.gender, item.birthYear, year, lifeExpectancies)))
+    }
 
 }
--- scala-csv-wrangling/src/test/scala/Tests.scala
+++ scala-csv-wrangling/src/test/scala/Tests.scala
@@ -40,10 +40,48 @@ class TestSuite extends org.scalatest.FunSuite {
         val x = SSARow(1910, "Jane", Female(), 5457)
         assert(mostPopular(List(a, s, r, t, u, w, x)) == ("Jane", 11383))
     }
-    test("findMax"){
-        val s = SSARow(1880, "Jane", Female(), 10)
-        val a = SSARow(1909, "Jane", Female(), 20)
-        assert(findMax(List(s, a)) == ("Jane", 30))
+    test("count"){
+        val s = SSARow(1880, "Jane", Female(), 4000)
+        val a = SSARow(1809, "Rando", Female(), 2000)
+        val r = SSARow(1809, "Rando", Female(), 2000)
+        val i = SSARow(1809, "Rando", Female(), 2000)
+        val m = SSARow(1809, "Rando", Female(), 2000)
+        assert(count(List(s, a, r, i, m)) == 12000)
+    }
+    test("countGirlsBoys"){
+        val s = SSARow(1880, "Mary", Female(), 7000)
+        val a = SSARow(1909, "Jane", Female(), 3000)
+        val r = SSARow(1900, "John", Male(), 3500)
+        val u = SSARow(1904, "William", Male(), 9000)
+        assert(countGirlsAndBoys(List(a, s, r, u)) == (10000, 12500))
+    }
+    test("genderNeutral"){
+        val s = SSARow(1880, "Pam", Female(), 7000)
+        val q = SSARow(1880, "Phil", Female(), 7000)
+        val w = SSARow(1880, "Phil", Male(), 7000)
+        val a = SSARow(1909, "Jane", Female(), 3000)
+        val r = SSARow(1900, "Pam", Male(), 3500)
+        val u = SSARow(1904, "William", Male(), 9000)
+        assert(genderNeutralNames(List(a, s, w, r, u, q)) == Set("Pam", "Phil"))
+    }
+    test("expectedAlive"){
+        val n = CDCRow(1930, 58, 62)
+        val s = CDCRow(1948, 61, 65)
+        val r = CDCRow(1997, 74, 79)
+        assert(expectedAlive(Male(), 1930, 2018, List(n, s, r)) == false)
+    }
+    test("estimatePopulation"){
+        val n = CDCRow(1930, 58, 62)
+        val p = CDCRow(1934, 58, 62)
+        val u = CDCRow(1958, 61, 65)
+        val y = CDCRow(1997, 74, 79)
+        val s = SSARow(1920, "Pam", Male(), 7000)
+        val q = SSARow(1934, "Phil", Male(), 7000)
+        val w = SSARow(1958, "Philly", Female(), 7000)
+        val a = SSARow(1997, "Jane", Female(), 3000)
+        val cdcr = List(n, p, u, y)
+        val ssr = List(a, s, w, q)
+        assert(estimatePopulation(ssr, 2018, cdcr) == 10000)
     }
 
 }
