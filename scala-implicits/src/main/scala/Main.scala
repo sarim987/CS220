0e538a5a3511cb39a63a3ad08d9320236f3b2237
Sarim Ahmed <Sarim@Sarims-MacBook-Pro.local>
2018-04-04 10:03:50 -0400

    update

--- scala-implicits/src/main/scala/Main.scala
+++ scala-implicits/src/main/scala/Main.scala
@@ -2,21 +2,57 @@ object PathImplicits {
 import java.nio.file._
 
     implicit class RString(me: String){
-    
-        def /(other: String) = Paths.get(me, other)
-        //def /(other:Path) = me.resolve(other)
-        
+        def /(other: String) = Paths.get(me, other)        
     }
-    implicit class LString(me: Path){
+    implicit class RPath(me: Path){
         def /(other:Path) = me.resolve(other)
         def write(other: String) = Files.write(me, other.getBytes())
         def read() = new String(Files.readAllBytes(me));
-        def append(other: String) = Files.write(me, other.getBytes(), StandardOpenOption.APPEND)
-        
+        def append(other: String) = Files.write(me, other.getBytes(), StandardOpenOption.APPEND)        
     }
-
 }
 
 object TimeImplicits {
+import java.time.LocalDate;
+    implicit class IntDate(me: Int){
+        def jan() = LocalDate.of(2018, 1, me)
+        def feb() = LocalDate.of(2018, 2, me)
+        def mar() = LocalDate.of(2018, 3, me)
+        def apr() = LocalDate.of(2018, 4, me)
+        def may() = LocalDate.of(2018, 5, me)
+        def jun() = LocalDate.of(2018, 6, me)
+        def jul() = LocalDate.of(2018, 7, me)
+        def aug() = LocalDate.of(2018, 8, me)
+        def sep() = LocalDate.of(2018, 9, me)
+        def oct() = LocalDate.of(2018, 10, me)
+        def nov() = LocalDate.of(2018, 11, me)
+        def dec() = LocalDate.of(2018, 12, me) 
+        
+        def jan(other:Int) = LocalDate.of(other, 1, me)
+        def feb(other:Int) = LocalDate.of(other, 2, me)
+        def mar(other:Int) = LocalDate.of(other, 3, me)
+        def apr(other:Int) = LocalDate.of(other, 4, me)
+        def may(other:Int) = LocalDate.of(other, 5, me)
+        def jun(other:Int) = LocalDate.of(other, 6, me)
+        def jul(other:Int) = LocalDate.of(other, 7, me)
+        def aug(other:Int) = LocalDate.of(other, 8, me)
+        def sep(other:Int) = LocalDate.of(other, 9, me)
+        def oct(other:Int) = LocalDate.of(other, 10, me)
+        def nov(other:Int) = LocalDate.of(other, 11, me)
+        def dec(other:Int) = LocalDate.of(other, 12, me)  
 
+        def days() = (me, "days")
+        def months() = (me, "months")
+        def years() = (me, "years")
+    }
+
+    implicit class AddDays(day: LocalDate){
+        def +(tup:(Int, String)) = {
+            if(tup._2.equals("days")){ day.plusDays(tup._1)}
+            else if(tup._2.equals("months")){ day.plusMonths(tup._1)}
+            else if(tup._2.equals("years")){ day.plusYears(tup._1)}
+            else throw new IllegalArgumentException("tuple 2 should be days, months, or years")
+        }
+
+    }
 }
--- scala-implicits/src/test/scala/Tests.scala
+++ scala-implicits/src/test/scala/Tests.scala
@@ -2,7 +2,7 @@ class Tests extends org.scalatest.FunSuite {
 
   import PathImplicits._
   import TimeImplicits._
-
+  import java.time.LocalDate;
   import java.nio.file._
 
   test("Path Test 1"){
@@ -25,6 +25,35 @@ class Tests extends org.scalatest.FunSuite {
 
   test("Append") {
     Paths.get("greeting2.txt").append("Hello There!\n")
+  }
+
+  test("Date") {
+    assert(12.jan == LocalDate.of(2018, 1, 12))
+    assert(12.jan(2018) == LocalDate.of(2018, 1, 12))
+    val a = 2 jan 2018
+    assert(a == LocalDate.of(2018, 1, 2))
+
+  }
+  test("addDays") {
+    //val a = LocalDate.of(2018, 1, 12)
+    //assert(a.days(2) == LocalDate.of(2018, 1, 14))
+    //val a = 10 days
+    //assert(a == 10)
+  }
+  test("PlusDays") {
+    val a = LocalDate.of(2018, 1, 12)
+    val b = a + 10.days
+    assert(b == LocalDate.of(2018, 1, 22))
+  }
+    test("PlusMonths") {
+    val a = LocalDate.of(2018, 1, 12)
+    val b = a + 10.months
+    assert(b == LocalDate.of(2018, 11, 12))
+  }
+      test("years") {
+    val a = LocalDate.of(2018, 1, 12)
+    val b = a + 2.years
+    assert(b == LocalDate.of(2020, 1, 12))
   }
 
 }
