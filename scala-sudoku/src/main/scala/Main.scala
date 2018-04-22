f862587d38c0cea48da948c722aa533e6f604eae
Sarim Ahmed <Sarim@Sarims-MacBook-Pro.local>
2018-04-21 22:18:02 -0400

    update

--- scala-sudoku/src/main/scala/Main.scala
+++ scala-sudoku/src/main/scala/Main.scala
@@ -63,7 +63,7 @@ class Board(val available: Map[(Int, Int), List[Int]]) extends BoardLike[Board] 
     def solRow(row: Int, col: Int): Boolean = {
       def solCol(col: Int): Boolean = {
         if(row == 8 && col == 8){if(valueAt(row, col) != None) true else false}
-        else if(col < 9){ if(valueAt(row, col) != None) solCol(col + 1) else false} 
+        else if(col < 8){ if(valueAt(row, col) != None) solCol(col + 1) else false} 
         else solRow(row + 1, 0)
       }
       solCol(0)
@@ -75,7 +75,7 @@ class Board(val available: Map[(Int, Int), List[Int]]) extends BoardLike[Board] 
     def solRow(row: Int, col: Int): Boolean = {
       def solCol(col: Int): Boolean = {
         if(row == 8 && col == 8){if(available(row, col).length == 0) true else false}
-        else if(col < 9){ if(available(row, col).length == 0) solCol(col + 1) else false} 
+        else if(col < 8){ if(available(row, col).length == 0) solCol(col + 1) else false} 
         else solRow(row + 1, 0)
       }
       solCol(0)
@@ -87,31 +87,51 @@ class Board(val available: Map[(Int, Int), List[Int]]) extends BoardLike[Board] 
     require(availableValuesAt(row, col).contains(value))
     val newBoard = new Board(available + ((row, col) -> (value :: Nil)))
     val lst = Solution.peers(row, col)
+    newBoard.removevals(lst, newBoard.available, value)
+  }
     def removevals(lst: Seq[(Int, Int)], valmap: Map[(Int, Int), List[Int]], value: Int): Board = lst match {
-      //case tup :: tail
-      case tup :: tail => removevals(tail, valmap + (tup -> valmap((tup._1,tup._2)).filter(_ != value)), value)
+      case tup :: tail if(valmap((tup._1,tup._2)).contains(value) && valmap((tup._1,tup._2)).size == 2) => place(tup._1, tup._2, new Board(available + ((tup._1,tup._2) -> available((tup._1,tup._2)).filter(_ != value))).available((tup._1,tup._2)).head)
+      case tup :: tail => new Board(available + ((tup._1,tup._2) -> available((tup._1,tup._2)).filterNot(_ == value))).removevals(tail, available + ((tup._1,tup._2) -> available((tup._1,tup._2)).filter(_ != value)), value)
       case Nil => new Board(valmap)
     }
-    removevals(lst, available + ((row, col) -> (value :: Nil)), value)
-  }
-
 
 
   // You can return any Iterable (e.g., Stream)
   def nextStates(): List[Board] = {
-    def nextBoards(lst: List[(Int, Int)]): List[Board] = /**lst match**/ {
-      //case tup :: tail => available.map(tup._1, tup._2) -> place(tup._1, tup._2, _)).toList ++ nextBoards(tail)
-      //case Nil => Nil
-      ???
+    def nextBoards(lst: List[(Int, Int)]): List[Board] = lst match {
+      case tup :: tail if available((tup._1, tup._2)).length > 1 => available((tup._1, tup._2)).map(x => place(tup._1, tup._2, x)) ++ nextBoards(tail)
+      case tup :: tail => nextBoards(tail)
+      case Nil => Nil
     }
     if (isUnsolvable()) {
       List()
     }
     else {
-      //nextBoards(0.to(8).flatMap(row => 0.to(8).map(col => (row, col))).toList)
-      ???
+      val rows = getTuples()
+      //println(rows)
+      nextBoards(rows)
     }
   }
 
-  def solve(): Option[Board] = ???
+   def getTuples(): List[(Int, Int)] = {
+    def solRow(row: Int, col: Int): List[(Int, Int)] = {
+      def solCol(col: Int): List[(Int, Int)] = {
+        if(row == 8 && col == 8){(row, col) :: Nil}
+        else if(col < 8){ (row, col) :: solCol(col + 1)} 
+        else (row, col) :: solRow(row + 1, 0)
+      }
+      solCol(0)
+    }
+    solRow(0, 0)
+  }
+
+
+  def solve(): Option[Board] = {
+    def solveBoard(board: List[Board]): Option[Board] = board match {
+      case Nil => None
+      case head :: tail => if(head.isSolved()) Some(head) else solveBoard(tail)
+    }
+  if(isSolved) Some(this) else solveBoard(nextStates())
+  }
+  
 }
--- scala-sudoku/src/test/scala/Tests.scala
+++ scala-sudoku/src/test/scala/Tests.scala
@@ -6,7 +6,7 @@ class TestSuite extends org.scalatest.FunSuite {
 
   test("EmptyBoard") {
   val str = "1................................................................................"
-    assert(Solution.parse(str) == new Board(Solution.calcAllPos(0).map(coord => coord -> 1.to(9).toList).toMap))
+    //assert(Solution.parse(str) == new Board(Solution.calcAllPos(0).map(coord => coord -> 1.to(9).toList).toMap))
   }
   test("valueAt1") {
   val str = ".17895632328764951569213784782349516145628397936157428891436275254971863673582149"
@@ -34,16 +34,34 @@ class TestSuite extends org.scalatest.FunSuite {
     val puzzle = "................................................................................."
     val board = Solution.parse(puzzle)
     board.place(0,0,5)
-    //println("BEASTMOOODEEE")
-    assert(board.place(0, 0, 3) == new Board(Solution.calcAllPos(0).map(coord => coord -> 1.to(9).toList).toMap))
+    //assert(board.place(0, 0, 3) == new Board(Solution.calcAllPos(0).map(coord => coord -> 1.to(9).toList).toMap))
   }
     test("place2") {
     val puzzle = "4.78.5632.2876495.5692.37847823495.6.45628.97936.5742889.436275.5497.86367.582.49"
     val board = Solution.parse(puzzle)
     
-    //println("BEASTMOOODEEE")
-    assert(board.place(0, 1, 1) == new Board(Solution.calcAllPos(0).map(coord => coord -> 1.to(9).toList).toMap))
+    //assert(board.place(0, 1, 1) == new Board(Solution.calcAllPos(0).map(coord => coord -> 1.to(9).toList).toMap))
   }
+    test("nextStates") {
+    val puzzle = "4.78.5632.2876495.5692.37847823495.6.45628.97936.5742889.436275.5497.86367.582.49"
+    val board = Solution.parse(puzzle)
+    
+    //assert(board.nextStates() == new Board(Solution.calcAllPos(0).map(coord => coord -> 1.to(9).toList).toMap))
+  }
+    test("solve") {
+    val puzzle = "4.78.5632.2876495.5692.37847823495.6.45628.97936.5742889.436275.5497.86367.582.49"
+    val board = Solution.parse(puzzle)
+    
+    //assert(board.solve() == Some(new Board(Solution.calcAllPos(0).map(coord => coord -> 1.to(9).toList).toMap)))
+  }
+  test("Easier1") {
+  // this one requires backtrack searching
+  val fromCS121_1 = "85....4.1......67...21....3..85....7...982...3....15..5....43...37......2.9....58"
+  val board = Solution.parse(fromCS121_1)
+  println(board.solve())
+  //assert(board.solve() == Some(new Board(Solution.calcAllPos(0).map(coord => coord -> 1.to(9).toList).toMap)))
+}
+
 
 
 
