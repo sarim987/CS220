f1a8023d08d8798730c1724517c373c905dc86dd
Sarim Ahmed <Sarim@Sarims-MacBook-Pro.local>
2018-05-06 22:54:52 -0400

    push

--- scala-parsing/src/main/scala/Solution.scala
+++ scala-parsing/src/main/scala/Solution.scala
@@ -2,24 +2,40 @@ import hw.parsing._
 import scala.util.parsing.combinator._
 
 object ArithEval extends ArithEvalLike {
-  def eval(e: Expr): Double = ???
+  def eval(e: Expr): Double = e match {
+    case Num(x) => x
+    case Exponent(x, y) => Math.pow(eval(x), eval(y))
+    case Mul(x, y) => eval(x) * eval(y)
+    case Div(x, y) => eval(x) / eval(y)
+    case Add(x, y) => eval(x) + eval(y)
+    case Sub(x, y) => eval(x) - eval(y) 
+  }
 }
 
 object ArithParser extends ArithParserLike {
 
   // number: PackratParser[Double] is defined in ArithParserLike
 
-  lazy val atom: PackratParser[Expr] = ???
+  lazy val atom: PackratParser[Expr] = "(" ~ expr ~ ")" ^^ { case _ ~ e ~ _ => e } | number ^^ { case x => Num(x) }
 
-  lazy val exponent: PackratParser[Expr] = ???
+  lazy val exponent: PackratParser[Expr] = exponent ~ "^" ~ atom ^^ { case e ~ _ ~ a => Exponent(e, a) } | atom
 
-  lazy val add: PackratParser[Expr] = ???
+  lazy val add: PackratParser[Expr] = add ~ "+" ~ mul ^^ { case a ~ _ ~ m => Add(a, m) } |
+                                      add ~ "-" ~ mul ^^ { case a ~ _ ~ m => Sub(a, m) } | mul
 
-  lazy val mul: PackratParser[Expr] = ???
+  lazy val mul: PackratParser[Expr] = mul ~ "*" ~ exponent ^^ { case m ~ _ ~ e => Mul(m, e) } |
+                                      mul ~ "/" ~ exponent ^^ { case m ~ _ ~ e => Div(m, e) } | exponent
 
-  lazy val expr: PackratParser[Expr] = ???
+  lazy val expr: PackratParser[Expr] = add
 }
 
 object ArithPrinter extends ArithPrinterLike {
-  def print(e: Expr): String = ???
+  def print(e: Expr): String = e match {
+    case Num(x) => x.toString()
+    case Exponent(x, y) => x.toString() + "^" + y.toString()
+    case Mul(x, y) => x.toString() + "*" + y.toString()
+    case Div(x, y) => x.toString() + "/" + y.toString()
+    case Add(x, y) => x.toString() + "+" + y.toString()
+    case Sub(x, y) => x.toString() + "-" + y.toString()
+  }
 }
--- scala-parsing/src/test/scala/Tests.scala
+++ scala-parsing/src/test/scala/Tests.scala
@@ -1,9 +1,30 @@
+import hw.parsing._
+import ArithEval._
+import ArithParser._
+import ArithPrinter._
+
 class TrivialTestSuite extends org.scalatest.FunSuite {
 
   test("several objects must be defined") {
     val parser: hw.parsing.ArithParserLike = ArithParser
     val printer: hw.parsing.ArithPrinterLike = ArithPrinter
     val eval: hw.parsing.ArithEvalLike = ArithEval
+    
   }
+
+  test("test1"){
+    assert(eval(parseArith("1+2/2+5*(2^2)+7-7/1*(18-17-1+5)/5*72/8+18/2*2^3-18")) == eval(Num(20)))
+  }
+  test("test1.5"){
+    assert(parseArith("1") == Num(1))
+  }
+  test("test2"){
+    assert(print(Num(1)) == "1.0")
+    assert(eval(Add(Num(1),Num(1))) == eval(Num(2)))
+  }
+  test("test3"){
+    assert(eval(parseArith("2^2")) == eval(Num(4)))
+  }
+
 
 }
